8b6c844e24e7212574afa5b1f760675ca2aec07b
= <=>
2019-02-13 17:59:33 -0300

    Aula 28 - Pacote Collective Form and HTML

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Pacote Collective Form and HTML
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -223,6 +226,11 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // Pacote Collective Form and HTML
+
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
--- resources/views/Painel/products/create-edit.blade.php
+++ resources/views/Painel/products/create-edit.blade.php
@@ -17,38 +17,45 @@
 @endif
 
 @if(isset($product))
-<form class="form" method="post" action="{{route('produtos.update', $product->id)}}">
-    {!! method_field('PUT') !!}
+<!-- <form class="form" method="post" action="{{route('produtos.update', $product->id)}}">
+    {!! method_field('PUT') !!} -->
+    {!! Form::model($product, ['route' => ['produtos.update', $product->id], 'class' => 'form', 'method' => 'put' ]) !!}
 @else
-<form class="form" method="post" action="{{route('produtos.store')}}">
+<!-- <form class="form" method="post" action="{{route('produtos.store')}}"> -->
+    {!! Form::open(['route' => 'produtos.store', 'class' => 'form']) !!}
 @endif
     <!-- <input type="hidden" name="_token" value="{{csrf_token()}}"> -->
-    {!! csrf_field() !!}
+    <!-- {!! csrf_field() !!} -->
     <div class="form-group">
         @if(isset($product))
-            <input type="text" name="name" placeholder="Nome:" class="form-control" value="{{old('name', $product->name)}}">
+            <!-- <input type="text" name="name" placeholder="Nome:" class="form-control" value="{{old('name', $product->name)}}"> -->
+            {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'nome:'])!!}
         @else
-            <input type="text" name="name" placeholder="Nome:" class="form-control" value="{{old('name')}}">
+            <!-- <input type="text" name="name" placeholder="Nome:" class="form-control" value="{{old('name')}}"> -->
+            {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'nome:'])!!}
         @endif
     </div>
 
     <div class="form-group">
         <label>
-            <input type="checkbox" name="active" value="1" @if(isset($product) && $product->active == '1') checked @endif>
+            <!-- <input type="checkbox" name="active" value="1" @if(isset($product) && $product->active == '1') checked @endif> -->
+            {!! Form::checkbox('active')!!}
             Ativo?
         </label>
     </div>
 
     <div class="form-group">
         @if(isset($product))
-            <input type="text" name="number" placeholder="Number:" class="form-control" value="{{old('number', $product->number)}}">
+            <!-- <input type="text" name="number" placeholder="Number:" class="form-control" value="{{old('number', $product->number)}}"> -->
+            {!! Form::text('number', null, ['class' => 'form-control', 'placeholder' => 'Número:'])!!}
         @else
-            <input type="text" name="number" placeholder="Number:" class="form-control" value="{{old('number')}}">
+            <!-- <input type="text" name="number" placeholder="Number:" class="form-control" value="{{old('number')}}"> -->
+            {!! Form::text('number', null, ['class' => 'form-control', 'placeholder' => 'Número:'])!!}
         @endif
     </div>
 
     <div class="form-group">
-        <select name="category" class="form-control">
+        <!-- <select name="category" class="form-control">
             <option value="">Escolha a categoria</option>
             @foreach($categorys as $category)
                 <option value="{{$category}}"
@@ -57,22 +64,27 @@
                     @endif
                 >{{$category}}</option>
             @endforeach
-        </select>
+        </select> -->
+        {!! Form::select('category', $categorys, null, ['class' => 'form-control']) !!}
     </div>
 
     <div class="form-group">
         @if(isset($product))
-            <textarea name="description" placeholder="Descrição" class="form-control">{{old('description', $product->description)}}</textarea>
+            <!-- <textarea name="description" placeholder="Descrição" class="form-control">{{old('description', $product->description)}}</textarea> -->
+            {!! Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'description:'])!!}
         @else
-            <textarea name="description" placeholder="Descrição" class="form-control">{{old('description')}}</textarea>
+            <!-- <textarea name="description" placeholder="Descrição" class="form-control">{{old('description')}}</textarea> -->
+            {!! Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'description:'])!!}
         @endif
     </div>
 
     @if(isset($product))
-        <button class="btn btn-primary">Editar</button>
+        <!-- <button class="btn btn-primary">Editar</button> -->
+        {!! Form::submit('Enviar', ['class' => 'btn btn-primary']) !!}
     @else
-        <button class="btn btn-primary">Cadastrar</button>
+        <!-- <button class="btn btn-primary">Cadastrar</button> -->
+        {!! Form::submit('Cadastrar', ['class' => 'btn btn-primary']) !!}
     @endif
 </form>
-
+{!! Form::close() !!}
 @endsection
